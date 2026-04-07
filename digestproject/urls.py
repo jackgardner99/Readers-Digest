e9fe626926c8db4975922016149cff5ac94a9e23
Jack Gardner <jtgardner99@gmail.com>
2026-04-07 15:23:55 -0500

    getting categories

--- digestproject/urls.py
+++ digestproject/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
+from digestapi.views import CategoryViewSet
 
 router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'categories', CategoryViewSet, 'category')
 
 urlpatterns = [
     path('', include(router.urls)),
