9a8a1891658907ed2d84bf7b028ab811a0373640
Jack Gardner <jtgardner99@gmail.com>
2026-04-08 16:08:57 -0500

    book review complete

--- digestapi/models/__init__.py
+++ digestapi/models/__init__.py
@@ -1,3 +1,4 @@
 from .book import Book
 from .category import Category
 from .book_category import BookCategory
+from .review import Review
--- seed_database.sh
+++ seed_database.sh
@@ -9,3 +9,4 @@ python3 manage.py loaddata users
 python3 manage.py loaddata tokens
 python3 manage.py loaddata books
 python3 manage.py loaddata categories
+python3 manage.py loaddata reviews
